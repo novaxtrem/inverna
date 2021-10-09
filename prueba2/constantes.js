7c9e1c8e7ea8adfd5af8e8b556bfb8132eca6bb9
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2021-10-09 14:33:11 -0300

    Update constantes.js

--- prueba2/constantes.js
+++ prueba2/constantes.js
@@ -7,12 +7,3 @@ const CONSULTO_MODULO = "php2/consulto-modulo.php";
 //INSERT - UPDATE
 const AGREGO_NUEVO_MODULO = "php2/agrego-nuevo-modulo.php";
 const CONFIGURO_MODULO_MISMO_TIPO = "php2/configuro-modulo.php";
-
-
-const SELECT = "php/select.php"; //SELECT
-const INSERT_USER_SCRIP_POST = "https://jap-site.000webhostapp.com/insert.php?"; //INSERT 
-const UPDATE_USER_SCRIPT_POST = "https://jap-site.000webhostapp.com/update.php?"; //UPDATE
-const INSERT_POST = "php/inserto.php"; // INSERT
-const CONSULTO_INVERNADEROS = "php/consulto-invernaderos.php";
-const CONSULTO_CONTROLADORAS = "php/consulto-controladoras.php";
-const AGREGO_INVERNADEROS = "php/agrego-invernaderos.php";
