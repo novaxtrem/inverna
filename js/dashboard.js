dce7182370341cdc3dae2584d87f780c1b8d0b76
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2021-10-17 16:10:18 -0300

    dfdf

--- js/dashboard.js
+++ js/dashboard.js
@@ -62,9 +62,111 @@ function dibujoModulos(arrayModulos) {
     $('#tabla-modulos-activos').html(htmlToAppend)
 
     $(".btn-configurar-modulo").click(function() {
-
+        consultomodulo(this);
+        dibujoPanelConfigurarModulo(modulo);
         $('#exampleModalCenter').modal('show');
     });
 
 }
+//
+
+//
+function consultomodulo(variable) {
+    var moduloID = variable.value;
+    $.ajax({
+        url: CONSULTO_MODULO,
+        method: 'POST',
+        async: false,
+        data: { id: moduloID },
+        dataType: "JSON",
+        success: function(data) {
+            modulo = {
+                id_modulo: data[0].id_modulo,
+                nombre: data[0].nombre,
+                lectura: data[0].lectura,
+                tipo: data[0].tipo,
+                estado: data[0].estado
+            };
+            if (jQuery.isEmptyObject(data)) {
+                alert("Empty Object");
+            }
+        }
+
+    });
+    return modulo;
+};
+//
+
+
+
+
+
+
+
+
+
+
+function dibujoPanelConfigurarModulo(modulo) {
+    //
+    var tipoModuloToAppend = `
+        <select class="uk-select" id="form-horizontal-select-tipo-configuro-modulo">
+            <option value="NO">Seleccionar tipo</option>
+            <option value="Humedad">Humedad</option>
+            <option value="Temperatura">Temperatura</option>
+            <option value="PH">PH</option>
+        </select>`;
+    //
+    htmlToAppend = `
+        <form class="uk-form-horizontal uk-margin-large">
+            <div class="uk-margin">
+                <h1 class="uk-heading-bullet" value="` + modulo.id_modulo + `">Cofigurar Modulo</h1>
+                <label class="uk-form-label" for="form-horizontal-text">ID MODULO</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-horizontal-text" type="text" value="` + modulo.id_modulo + `" placeholder="` + modulo.id_modulo + `" disabled>
+                </div>
+            </div>
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-horizontal-select-tipo">TIPO MODULO</label>
+                <div class="uk-form-controls">` + tipoModuloToAppend + `
+                </div>
+            </div>
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-horizontal-text">LECTURA</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-horizontal-text" type="text" value="` + modulo.lectura + `" placeholder="` + modulo.lectura + `" disabled>
+                </div>
+            </div>
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-horizontal-text">OBJETIVO</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-horizontal-text-valor-objetivo" type="text" placeholder="ingrese valor deseado">
+                </div>
+            </div>
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-horizontal-select-accion">ACCION</label>
+                <div class="uk-form-controls">
+                    <select class="uk-select" id="form-horizontal-select-configuro-accion">
+                        <option value="ON">encender</option>
+                        <option value="OFF">apagar</option>
+                    </select>
+                </div>
+            </div>
+            <p uk-margin>
+                <button class="uk-button uk-button-default uk-button-small">CANCELAR</button>
+                <button class="uk-button uk-button-primary uk-button-small" onclick="agregoActualizoConfiguracion(modulo)">ACEPTAR</button>  
+            </p>
+        </form>`;
+    //
+    $('#configurar-modulo-container').html(htmlToAppend);
+    //
+    if (typeof modulo.tipo != 'undefined' || modulo.tipo != null) {
+        var options = document.getElementById('form-horizontal-select-tipo-configuro-modulo').options;
+        for (let i = 0; i < options.length; i++) {
+            if (modulo.tipo == options[i].value) {
+                document.getElementById("form-horizontal-select-tipo-configuro-modulo").options[i].selected = 'selected';
+            }
+        }
+        //$('#form-horizontal-select-tipo').prop('disabled', true);
+    }
+};
 //
