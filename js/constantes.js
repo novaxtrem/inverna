a013e472d2c0bc2496a0aba65b5db563df09130f
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2021-10-23 12:02:44 -0300

    Update constantes.js

--- js/constantes.js
+++ js/constantes.js
@@ -1,6 +1,6 @@
 //CONSULTO INFO
-const GET_CONSULTO_MODULOS = "http://192.168.0.155/inverna/php/consulto-modulos-activos.php"; //laburo
-//const GET_CONSULTO_MODULOS = "http://192.168.1.5/inverna/php/consulto-modulos-activos.php"; //casa
+//const GET_CONSULTO_MODULOS = "http://192.168.0.155/inverna/php/consulto-modulos-activos.php"; //laburo
+const GET_CONSULTO_MODULOS = "http://192.168.1.2/inverna/php/consulto-modulos-activos.php"; //casa
 const CONSULTO_MODULO = "php/consulto-modulo.php";
 
 //INSERT - UPDATE
